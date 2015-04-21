77f25320d0192745af3af3dbf0ba13e15f1e5f77
Filchakov <filchakov.denis@gmail.com>
2015-04-21 12:15:57 +0300

    work commit

--- index.rb
+++ index.rb
@@ -36,6 +36,7 @@ class Login < ActiveRecord::Base
 	belongs_to :user
 end
 
+#добавить учет расхода за смс, по каждому терминалу
 
 =begin
 Генерируем код для авторизации
@@ -46,21 +47,21 @@ post '/login' do
 	return {:error => 1, :code => 1, :msg => 'not_number_or_terminal'}.to_json if params[:number].nil? || params[:terminal].nil?
 	sms = SMSC.new()
 	user = User.find_by_number(params[:number])
-	user = User.create :number => params[:number], :date_create => DateTime.now if user.nil?
+	user = User.create :number => params[:number], :date_create => Time.now.utc if user.nil?
 	terminal = Terminal.find_by_token(params[:terminal])
 	sms_rand = rand(100..999)
 	sms.send_sms(user.number, "Ваш пароль: #{sms_rand}", 1)
-	login = Login.create :user_id => user.id, :terminal => terminal, :session => Digest::MD5.hexdigest("#{user.number} #{DateTime.now}"), :sms_code => sms_rand, :expiration_date => DateTime.now + 5.minute
+	login = Login.create :user_id => user.id, :terminal => terminal, :session => Digest::MD5.hexdigest("#{user.number} #{Time.now.utc}"), :sms_code => sms_rand, :expiration_date => Time.now.utc + 5.minute
 	login.to_json
 end
 
 
 #curl -X POST -H "Content-type: application/x-www-form-urlencoded" "http://localhost:4567/active_sms" -d 'sms_code=866&session=03455011529328e6aca83e3f9dc52ac3'
 post '/active_sms' do
-	login = Login.where("sms_code = ? AND session = ? AND expiration_date >= ? AND status != 1", params[:sms_code], params[:session], DateTime.now)
+	login = Login.where("sms_code = ? AND session = ? AND expiration_date >= ? AND status != 1", params[:sms_code], params[:session], Time.now.utc)
 	unless login.nil? || login.count !=1
 		login.first.status = 1 
-		login.first.expiration_date = DateTime.now + 1.minute
+		login.first.expiration_date = Time.now.utc + 1.minute
 		login.first.save
 		return login.first.to_json
 	else
@@ -68,8 +69,9 @@ post '/active_sms' do
 	end
 end
 
+#curl -X POST -H "Content-type: application/x-www-form-urlencoded" "http://localhost:4567/check_timeout" -d 'session=b0b967ff51456c060ed33dd243623350'
 post '/check_timeout' do
-	check_timeout
+	return check_timeout.to_json
 end
 
 post '/update_session' do
@@ -77,17 +79,20 @@ post '/update_session' do
 end
 
 def check_timeout
-	return true if Login.where("session = ? AND status = 1 AND expiration_date >= ?", params[:session], DateTime.now).count >= 1
-	return false
+	return Login.where("session = ? AND status = 1 AND expiration_date >= ?", params[:session], Time.now.utc).count == 1
 end
 
 def update_session
-	session = Login.find_by_session(params[:session])
-	unless session.nil?
-		session.expiration_date = DateTime.now + 1.minute
-		session.save
+	puts Time.now.utc
+	if check_timeout
+		session = Login.find_by_session(params[:session])
+		unless session.nil?
+			session.update_attributes(:expiration_date => Time.now.utc + 1.minute)
+			session.save
+		end
+		return session.to_json
 	end
-	session.to_json
+	return {:error => 1, :code => 3, :msg => 'token_expiration_failed'}.to_json
 end
 
 
